17f4548f48b9c52641ce27551b4064e5cac8c7c8
Serhii120796 <sfedyura@ukr.net>
2023-10-28 21:25:32 +0300

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,11 +1,11 @@
 import { Layout } from './Layout';
 import { Route, Routes } from 'react-router-dom';
-import Home from '../pages/Home/Home';
-import Movies from '../pages/Movies';
-import MovieDetails from '../pages/MovieDetails/MovieDetails';
+import Home from 'pages/Home/Home';
+import Movies from 'pages/Movies';
+import MovieDetails from 'pages/MovieDetails/MovieDetails';
 import NotFound from 'pages/NoteFound/NotFound';
-import Cast  from '../pages/Cast/Cast';
-import Reviews from '../pages/Reviews/Reviews';
+import Cast from 'pages/Cast/Cast';
+import Reviews from 'pages/Reviews/Reviews';
 import { Link, Navigation, Header} from './App.styled';
 
 export const App = () => {
